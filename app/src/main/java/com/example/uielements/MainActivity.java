b686c556f10cb259f85a0e292ec00a322950f895
Ian Chai <ianchai@mmu.edu.my>
2021-02-17 16:12:21 +0800

    Add code for the check boxes

--- app/src/main/java/com/example/uielements/MainActivity.java
+++ app/src/main/java/com/example/uielements/MainActivity.java
@@ -3,12 +3,60 @@ package com.example.uielements;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CheckBox cGodFather, cPulp, cMatrix;
+    private RadioGroup radioGroup;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cGodFather=findViewById(R.id.godfather);
+        cPulp=findViewById(R.id.pulp);
+        cMatrix=findViewById(R.id.matrix);
+
+        radioGroup=findViewById(R.id.radgrp);
+
+        progressBar=findViewById(R.id.progressBar);
+
+        cGodFather.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (cGodFather.isChecked())
+                    Toast.makeText(MainActivity.this, "You have selected The Godfather",Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(MainActivity.this, "You have NOT selected The Godfather",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        cPulp.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (cPulp.isChecked())
+                    Toast.makeText(MainActivity.this, "You have selected Pulp Fiction",Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(MainActivity.this, "You have NOT selected Pulp Fiction",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        cMatrix.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (cMatrix.isChecked())
+                    Toast.makeText(MainActivity.this, "You have selected The Matrix",Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(MainActivity.this, "You have NOT selected The Matrix",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
